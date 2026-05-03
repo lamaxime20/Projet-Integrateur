44d0e04940c9b5468e385bb55a939a114276d92e
lamaxime20 <lamaxime20@gmail.com>
2026-05-04 00:08:57 +0200

    Déployement backend

--- vue/src/utils/config.js
+++ vue/src/utils/config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000/api';
+// const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://projet-integrateur-do3r.onrender.com/api';
 
 export default API_BASE_URL;
