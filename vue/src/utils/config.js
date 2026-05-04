3bf4a2c50c676d31cb97f90c616029f86e96875d
lamaxime20 <lamaxime20@gmail.com>
2026-05-04 14:20:38 +0200

    Fichier pour qfficher les erreurs

--- vue/src/utils/config.js
+++ vue/src/utils/config.js
@@ -1,4 +1,4 @@
-// const API_BASE_URL = 'http://localhost:8000/api';
-const API_BASE_URL = 'https://amused-presence-production-a3ec.up.railway.app/api';
+const API_BASE_URL = 'http://localhost:8000/api';
+// const API_BASE_URL = 'https://amused-presence-production-a3ec.up.railway.app/api';
 
 export default API_BASE_URL;
