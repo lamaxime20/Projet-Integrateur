7aa3b005d3c53e98575aea21d2dd49f1e879d409
lamaxime20 <lamaxime20@gmail.com>
2026-05-08 08:11:26 +0200

    Session secure

--- backend/config/session.php
+++ backend/config/session.php
@@ -169,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -200,7 +200,7 @@ return [
     */
 
     'same_site' => env('SESSION_SAME_SITE', 'lax'),
-
+    
     /*
     |--------------------------------------------------------------------------
     | Partitioned Cookies
