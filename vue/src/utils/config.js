c56e31a9b39ad7ae52d1c1a28318712466cf263e
lamaxime20 <lamaxime20@gmail.com>
2026-05-08 05:54:50 +0200

    Dockerfie reverb mis à jour

--- vue/src/utils/config.js
+++ vue/src/utils/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000/api';
-// const API_BASE_URL = 'https://projet-integrateur-ngg1.onrender.com/api';
+const API_BASE_URL = 'https://projet-integrateur-ngg1.onrender.com/api';
+// const API_BASE_URL = 'http://localhost:8000/api';
 
 export default API_BASE_URL;
--- vue/src/utils/echo.js
+++ vue/src/utils/echo.js
@@ -1,5 +1,6 @@
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
+import API_BASE_URL from './config.js';
 
 window.Pusher = Pusher;
 
@@ -7,11 +8,11 @@ const echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST ?? 'localhost',
-    wsPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8080),
-    wssPort: Number(import.meta.env.VITE_REVERB_PORT ?? 8080),
+    wsPort: Number(import.meta.env.VITE_REVERB_PORT ?? 80),
+    wssPort: Number(import.meta.env.VITE_REVERB_PORT ?? 443),
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
     enabledTransports: ['ws', 'wss'],
-    authEndpoint: 'http://localhost:8000/api/broadcasting/auth',
+    authEndpoint: `${API_BASE_URL}/broadcasting/auth`,
     auth: {
         withCredentials: true,
     },
